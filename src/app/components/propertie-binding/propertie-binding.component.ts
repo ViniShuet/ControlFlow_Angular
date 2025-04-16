9d9720dc6a24a5c941f02c6ee5216e3e576db71b
ViniShuet <labsfiap@fiap.com.br>
2025-04-16 20:12:28 -0300

    Adicionar imagem

---
+++ src/app/components/propertie-binding/propertie-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propertie-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './propertie-binding.component.html',
+  styleUrl: './propertie-binding.component.css'
+})
+export class PropertieBindingComponent {
+ img = "../../../assets/Paisagem-1.webp"
+ isDisabled = true
+ descImag = "paisagem"
+}
